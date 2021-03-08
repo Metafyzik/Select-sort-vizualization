dd2f0ac1ae72a45165c0c5a6b9e34fab00e301a3
Metafyzik <75122971+Metafyzik@users.noreply.github.com>
2021-03-08 23:22:19 +0100

    Update select_sort_viz.py

--- select_sort_viz.py
+++ select_sort_viz.py
@@ -1,39 +1,42 @@
 
-#import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 from numpy import random
 
+# creating array to be sorted 
 N = 400
 x = np.arange(3,N,8)
-y = [num+10*random.rand() for num in x]
-
+y = x.copy()
 random.shuffle(y)
-colors = ["black",]*len(x)
 
 
+colors_init = ["black",]*len(x)
+color_curr =["rgb(192,192,192)",]*len(x)
 
-def SelectSort(pole):
+def SelectSort(array):
     k=1
-    for i in range(0, len(pole)):
+    for i in range(0, len(array)):
         k=i
-        
-        for j in range(i+1, len(pole)):
-            colors = ["rgb(192,192,192)",]*len(pole)
-            colors[j] = "crimson"
-            yield pole,colors
-            if pole[j]<pole[k]:
+        for j in range(i+1, len(array)):
+
+            colors = color_curr
+            colors[j] = "crimson" # coloring current quantity
+            yield array,colors 
+
+            colors[j] = "rgb(192,192,192)" # changing color back
+            if array[j]<array[k]:
                 k=j
-        pole[i],pole[k]=pole[k],pole[i]
-        colors = ["rgb(192,192,192)",]*len(pole)
+
+        array[i],array[k]=array[k],array[i]
         colors[i] = "purple"
-        yield pole,colors
-    # sorted state    
-    colors = ["white",]*len(pole)
-    yield pole,colors    
+        yield array,colors
+        colors[i] = "rgb(192,192,192)"# changing color back
+     
+    colors = ["white",]*len(array)# sorted state, last frame  
+    yield array,colors    
 
 
-def frame_args(duration):
+def animation_speed(duration):
     return {
             "frame": {"duration": duration,"redraw": False},
             "mode": "immediate",
@@ -47,13 +50,11 @@ sliders = [
                 "len": 0.5,
                 "x": 0.2,
                 "y": 0,
-                "active" : 10,
-                #"currentvalue" : False, #{"prefix": "speed: "},
-                #"visible"
+                "active" : 10,                
                 "steps": [
                     {   
-                        "args": [None, frame_args(speed)],
-                        "label":  " ", #! Cant you just scratch
+                        "args": [None,  animation_speed(speed)],
+                         "label":  " ", 
                         "method": "animate",
                     }
                     for speed in np.arange(500, 0, -20)
@@ -61,13 +62,10 @@ sliders = [
             }
         ]
 
-
 # Create figure
 fig = go.Figure(
-    data=[go.Bar(x=x, y=y,marker_color=colors,
+    data=[go.Bar(x=x, y=y,marker_color=colors_init, # initial vizuals
         marker_line_color='rgb(0,0,0)',marker_line_width=1.5),
-    
-
           ],
     layout=go.Layout(
         xaxis=dict(range=[-1, N], autorange=False, zeroline=False, showgrid=False,visible= False),
@@ -80,10 +78,10 @@ fig = go.Figure(
                             y=-0.05,
                           buttons=[dict(label="&#9654;", # play button
                                         method="animate",
-                                        args=[None, frame_args(200)] ), 
+                                        args=[None, animation_speed(200)] ), 
                                    dict(label="&#9724;", # stop button
                                         method="animate",
-                                        args=[[None], frame_args(0)] ),
+                                        args=[[None], animation_speed(0)] ),
                                    
                                    ],
                                    ), ],
@@ -92,21 +90,15 @@ fig = go.Figure(
     frames=[go.Frame(
         data=[dict(type="bar",
             x=x,
-            y=output[0], #!
+            y=output[0], 
             marker_color=output[1],
             marker_line_color="rgb(0,0,0)",
             marker_line_width=1.5,
-            
 
-            ), ],) #! Dont use it purposly or scrape it.
-
+            ), ],)
         for output in SelectSort(y)
         ],
 
-        
 )
-#print(fig)
+
 fig.show()
-
-
-#print(colors)
